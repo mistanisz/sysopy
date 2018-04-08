49ae271f3cc4dd87eaef740cd7219e041f97d8ce
Michał Stanisz <mch.stanisz@gmail.com>
2018-04-08 15:02:25 +0200

    Finished zad1

---
+++ zestaw4/zad1.c
@@ -0,0 +1,48 @@
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+pid_t pid;
+int state = 0;
+
+void sigint(int signo){
+    printf("\nOdebrano sygnał SIGINT\n");
+    kill(pid, SIGINT);
+    exit(0);
+}
+
+void sigtstp(int signo){
+    if(!state){
+        printf("\nOczekuję na CTRL+Z - kontynuacja albo CTR+C - zakonczenie programu\n");
+        state = 1;
+        kill(pid, SIGINT);
+    }
+    else{
+        pid = fork();
+        if(!pid){
+            execl("date.sh", "date.sh", NULL);
+        }
+        state = 0;
+    }
+}
+
+int main(){
+    struct sigaction act;
+    act.sa_handler = sigtstp;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    signal(SIGINT, &sigint);
+    sigaction(SIGTSTP, &act, NULL);
+    pid = fork();
+    if(!pid){
+        execl("date.sh", "date.sh", NULL);
+    }
+    while(1)sleep(1);
+}
